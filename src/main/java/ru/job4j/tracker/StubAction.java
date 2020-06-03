a308010fbf28a18d45dbd9eaf703352364d60cd7
Volk <denvolk91@gmail.com>
2020-06-03 17:02:08 +0300

    Написать тесты на StartUI. [#250797]

---
+++ src/main/java/ru/job4j/tracker/StubAction.java
@@ -0,0 +1,20 @@
+package ru.job4j.tracker;
+
+public class StubAction implements UserAction {
+    private boolean call = false;
+
+    @Override
+    public String name() {
+        return "Stab action";
+    }
+
+    @Override
+    public boolean execute(Input input, Tracker tracker) {
+        call = true;
+        return false;
+    }
+
+    public boolean isCall() {
+        return call;
+    }
+}
--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -10,35 +10,12 @@ import static org.junit.Assert.*;
 
 public class StartUITest {
     @Test
-    public void whenAddItem() {
-        String[] answers = {"Fix PC"};
-        Input input = new StubInput(answers);
-        Tracker tracker = new Tracker();
-        StartUI.createItem(input, tracker);
-        Item created = tracker.findAll()[0];
-        Item expected = new Item("Fix PC");
-        assertThat(created.getName(), is(expected.getName()));
-    }
-    @Test
-    public void whenReplaceItem() {
-        Tracker tracker = new Tracker();
-        Item item = new Item("new item");
-        tracker.add(item);
-        String[] answers = {
-                item.getId(), // id сохраненной заявки в объект tracker.
-                "replaced item"
-        };
-        StartUI.replaceItem(new StubInput(answers), tracker);
-        Item replaced = tracker.findById(item.getId());
-        assertThat(replaced.getName(), is("replaced item"));
-    }
-    @Test
-    public void whenDeleteItem() {
-        Tracker tracker = new Tracker();
-        Item item = new Item("New item");
-        tracker.add(item);
-        String[] answers = {item.getId(), "delete item"};
-        StartUI.deleteItem(new StubInput(answers), tracker);
-        assertThat(tracker.findById(item.getId()), is(nullValue()));
+    public void whenExit() {
+        StubInput input = new StubInput(
+                new String[] {"0"}
+        );
+        StubAction action = new StubAction();
+        new StartUI().init(input, new Tracker(), new UserAction[] { action });
+        assertThat(action.isCall(), is(true));
     }
 }
